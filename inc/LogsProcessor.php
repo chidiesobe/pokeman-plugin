2b1c36862fac44afbb1d12cde3510bb05ea92389
Chidi E <chidiesobe@gmail.com>
2023-12-10 17:39:07 +0000

    Code refactoring: created the LogPrecessor and SanitiseProcessor files, the log and clean the id

---
+++ inc/LogsProcessor.php
@@ -0,0 +1,64 @@
+<?php
+require_once('MessageDisplay.php');
+
+class LogsProcessor
+{
+    private $msgDisplay;
+    public $getLogFilePath;
+
+    public function __construct()
+    {
+        $this->msgDisplay = new MessageDisplay();
+        $this->getLogFilePath = $this->getLogFile();
+    }
+
+    private function getLogFile(): string
+    {
+        return  dirname(plugin_dir_path(__FILE__)) . '/logs/logs.txt';
+    }
+
+    private function writeToFile(string $content): bool
+    {
+        $file = fopen($this->getLogFilePath, 'a');
+        if ($file) {
+            fwrite($file, $content . PHP_EOL);
+            fclose($file);
+            return true;
+        }
+        return false;
+    }
+
+    public function writeToLog(int $id): void
+    {
+        $this->writeToFile($id);
+    }
+
+
+    public function processLogs(string $logs): bool
+    {
+        $currentUser = wp_get_current_user();
+
+        if (wp_verify_nonce($_POST['pokeLogNonce'], 'verifyLogIDs') && current_user_can('manage_options')) {
+
+            // Format Logs
+            $formatLogs = preg_replace('/\h+/', ' ', $logs);
+            $formatLogs = preg_replace('/(?<=\d)\s+(?=\d)/m', "\n", $formatLogs);
+            $formattedLogs = ltrim($formatLogs) . "\n";
+
+            if ($this->writeToFile($formattedLogs)) {
+                // Clear log file before writing new content to it
+                file_put_contents($this->getLogFilePath, '');
+                file_put_contents($this->getLogFilePath, $formattedLogs);
+
+                $this->msgDisplay->showMessage('', '', 'Log file has been updated successfully!', 'success',  []);
+                return true;
+            } else {
+                $this->msgDisplay->showMessage('', '', 'Failed to format log file!', 'warning',  []);
+                return false;
+            }
+        } else {
+            $this->msgDisplay->showMessage('Sorry', $currentUser->display_name, ' but you are not authorized to carry out this action', 'danger', []);
+            return false;
+        }
+    }
+}
---
+++ inc/SanitiseProcessor.php
@@ -0,0 +1,55 @@
+<?php
+require_once('LogsProcessor.php');
+
+class SanitiseProcessor
+{
+    private $numericValues;
+    public $nonNumericalValues;
+
+    public function __construct()
+    {
+        $this->logsProcessor = new LogsProcessor();
+    }
+    public function getNumericValues(): string
+    {
+        return $this->numericValues;
+    }
+
+    public function setPokemonIDs(string $ids): void
+    {
+        $this->cleanPokemanID($ids);
+    }
+
+    private function cleanPokemanID(string $ids = ""): void
+    {
+        // remove white space and ensure string integrity
+        $input_ids = str_replace(' ', '', sanitize_text_field($ids));
+        $input_ids = str_replace('.', '', sanitize_text_field($input_ids));
+
+        // Check if the string starts or ends with a comma and remove it
+        if (substr($input_ids, 0, 1) === ',')  $input_ids = substr($input_ids, 1);
+        if (substr($input_ids, -1) === ',')  $input_ids = substr($input_ids, 0, -1);
+
+        // Remove all non numberic values
+        $exploded_string = explode(',', $input_ids);
+
+        // Remove duplicates
+        $unique_values = array_unique($exploded_string);
+
+        // Remove numbers that exist in the logs file
+        $log_numbers = file($this->logsProcessor->getLogFilePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        $non_logged_number = array_filter($unique_values, function ($values) use ($log_numbers) {
+            return !in_array($values, $log_numbers);
+        });
+
+        // Seperate numerical and non numerical values 
+        $this->numericValues = array_filter($non_logged_number, function ($value) {
+            return is_numeric($value);
+        });
+        $this->nonNumericalValues = array_diff($non_logged_number, $this->numericValues);
+
+        $clean_ids = implode(',', $this->numericValues);
+        $this->numericValues = $clean_ids;
+    }
+}
